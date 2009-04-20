5b7cd777a27cdd39e5f71cc7d763a999f7830e63
agentzh <agentzh@gmail.com>
2009-04-20 19:01:36 +0800

    added newlines

--- lib/SSH/Batch.pm
+++ lib/SSH/Batch.pm
@@ -88,6 +88,7 @@ If you have a branch for me to pull, please let me know ;)
 This module as well as its programs are licensed under the BSD License.
 
 Copyright (c) 2009, Yahoo! China EEEE Works, Alibaba Inc. All rights reserved.
+
 Copyright (C) 2009, Agent Zhang (agentzh). All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
