ab16fe77190d0d1909925f8b4bfc09fd3947e0c7
agentzh <agentzh@gmail.com>
2009-04-26 12:58:45 +0800

    CPAN release 0.017

--- lib/SSH/Batch.pm
+++ lib/SSH/Batch.pm
@@ -3,7 +3,7 @@ package SSH::Batch;
 use strict;
 use warnings;
 
-our $VERSION = '0.016';
+our $VERSION = '0.017';
 
 1;
 __END__
--- lib/SSH/Batch/ForNodes.pm
+++ lib/SSH/Batch/ForNodes.pm
@@ -3,7 +3,7 @@ package SSH::Batch::ForNodes;
 use strict;
 use warnings;
 
-our $VERSION = '0.016';
+our $VERSION = '0.017';
 
 use Set::Scalar;
 use File::HomeDir;
