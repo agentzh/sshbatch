b87e76bd845ae52501134fcbb50801c0aeb1c887
agentzh (章亦春) <agentzh@gmail.com>
2010-01-04 10:09:07 +0800

    added one more example for cluster variables to keynodes' POD, thanks cnhackTNT.

--- lib/SSH/Batch.pm
+++ lib/SSH/Batch.pm
@@ -3,7 +3,7 @@ package SSH::Batch;
 use strict;
 use warnings;
 
-our $VERSION = '0.022';
+our $VERSION = '0.023';
 
 1;
 __END__
@@ -14,7 +14,7 @@ SSH::Batch - Cluster operations based on parallel SSH, set and interval arithmet
 
 =head1 VERSION
 
-This document describes SSH::Batch 0.022 released on Sep 23, 2009.
+This document describes SSH::Batch 0.023 released on Jan 4, 2010.
 
 =head1 SYNOPSIS
 
--- lib/SSH/Batch/ForNodes.pm
+++ lib/SSH/Batch/ForNodes.pm
@@ -3,7 +3,7 @@ package SSH::Batch::ForNodes;
 use strict;
 use warnings;
 
-our $VERSION = '0.022';
+our $VERSION = '0.023';
 
 use Set::Scalar;
 use File::HomeDir;
